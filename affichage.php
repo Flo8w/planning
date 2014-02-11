70ff629f93a0112c8c09411ccff25cf090760359
lecompte <kilian.lecompte@etu.univ-orleans.fr>
2014-02-11 09:15:26 +0100

    Resolution bug affichage

--- affichage.php
+++ affichage.php
@@ -1,14 +1,6 @@
 <?php
 //Definition des constantes et des variables
 require_once('connect.php');
-$dsn="mysql:dbname=".BASE.";host=".SERVER;
-try{
-  $connexion=new PDO($dsn,USER,PASSWD);
-}
-catch(PDOException $e){
-  printf("Echec de la connexion : %s\n", $e->getMessage());
-  exit();
-}
 
 $errorMessage = '';
 
@@ -27,36 +19,6 @@ if(!empty($_POST))
      $_SESSION['password'] = $_POST['password'];
      //On redirige vers le fichier planning.php
      header('Location: planning.php');
-
-     /*
-     $sql="SELECT * from creer where utilisateur=(select id from utilisateur where login=:login and password=:password)";
-     $stmt=$connexion->prepare($sql);
-     $stmt->bindParam(':login',$_POST['login']);
-     $stmt->bindParam(':password',$_POST['password']);
-     $stmt->execute();
-
-     if($stmt->rowCount() == 0)
-     {
-       $errorMessage = "Aucune activité programmée";
-     }
-     else //Tout va bien
-     {
-
-       echo $_POST['login']."<br/>";
-       foreach($stmt as $plan){
-	 echo $plan[2]."h : ".$plan[1]."<br/>";
-       }
-
-       
-       //On ouvre la session
-       session_start();
-       //On enregistre le login en session
-       $_SESSION['login'] = $_POST['login'];
-       //On redirige vers le fichier planning.php
-       header('Location: planning.php');
- 
-       
-     }*/
    }
    else
    {
--- planning.php
+++ planning.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 
+require_once ("config.php");
+
 $sql="SELECT * from creer where utilisateur=(select id from utilisateur where login=:login and password=:password)";
 $stmt=$connexion->prepare($sql);
 $stmt->bindParam(':login',$_SESSION['login']);
@@ -8,18 +10,17 @@ $stmt->bindParam(':password',$_SESSION['password']);
 $stmt->execute();
 
 if($stmt->rowCount() == 0)
-  {
-    $errorMessage = "Aucune activité programmée";
-  }
+{
+  echo "Aucune activité programmée";
+}
 else //Tout va bien
-  {
+{
+  echo $_SESSION['login']."<br/>";
+  foreach($stmt as $plan){
+    echo $plan[2]."h : ".$plan[1]."<br/>";
+  }     
+}
 
-    echo $_POST['login']."<br/>";
-    foreach($stmt as $plan){
-      echo $plan[2]."h : ".$plan[1]."<br/>";
-    }
-       
-  }
 ?>
 
 <form action="affichage.php" method="POST">
